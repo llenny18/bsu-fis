bbea558bd314f89f4023f4cd79e22b044a920541
Allen Eidrian Silva Ramos <115854514+llenny18@users.noreply.github.com>
2025-01-09 13:18:40 +0800

    to php

--- index.php
+++ index.php
@@ -57,18 +57,11 @@
                         <a href="index.html">
                             <b class="logo-icon">
                                 <!-- Dark Logo icon -->
-                                <img src="assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+                                <img src="assets/images/FISlogo.png" style="width: 220px;" alt="homepage" class="dark-logo" />
                                 <!-- Light Logo icon -->
-                                <img src="assets/images/logo-icon.png" alt="homepage" class="light-logo" />
+                                <img src="assets/images/FISlogo.png" alt="homepage" class="light-logo" />
                             </b>
-                            <!--End Logo icon -->
-                            <!-- Logo text -->
-                            <span class="logo-text">
-                                <!-- dark Logo text -->
-                                <img src="assets/images/logo-text.png" alt="homepage" class="dark-logo" />
-                                <!-- Light Logo text -->
-                                <img src="assets/images/logo-light-text.png" class="light-logo" alt="homepage" />
-                            </span>
+                            
                         </a>
                     </div>
                     <!-- ============================================================== -->
